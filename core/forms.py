a83ae180e003e207ba77034315368bb00cf6ca35
TatianaMraz <tatiana.mrazova@icloud.com>
2024-06-05 14:00:22 +0200

    Supplier form - created

--- core/forms.py
+++ core/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 
-from core.models import Product
+from core.models import Product, Supplier
 
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
+        fields = '__all__'
+
+
+class SupplierForm(forms.ModelForm):
+    class Meta:
+        model = Supplier
         fields = '__all__'
