f8c1fa639f20794f7f1c2cc19546766c62cdd5ab
TatianaMraz <tatiana.mrazova@icloud.com>
2024-06-20 15:17:24 +0200

    Product model - ico/dic modified

--- core/models.py
+++ core/models.py
@@ -32,8 +32,8 @@ class TableHead(models.Model):
 class Supplier(models.Model):
     company = models.CharField(max_length=100, unique=True)
     address = models.CharField(max_length=255)
-    ico = models.CharField(max_length=10)
-    dic = models.CharField(max_length=10, blank=True, null=True)
+    ico = models.CharField(max_length=20)
+    dic = models.CharField(max_length=50, blank=True, null=True)
     bank_account = models.CharField(max_length=100, blank=True, null=True)
     note = models.CharField(max_length=255, blank=True, null=True)
     created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, related_name='supplier_created_by', null=True, blank=True)
