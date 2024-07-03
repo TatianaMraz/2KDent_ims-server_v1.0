c907635e42ff0071957caff0c3bcba3ecaf81998
TatianaMraz <tatiana.mrazova@icloud.com>
2024-07-03 14:27:10 +0200

    Manufacturer errro message updated

--- core/views.py
+++ core/views.py
@@ -117,6 +117,22 @@ class ManufacturerViewSet(viewsets.ModelViewSet):
     def perform_update(self, serializer):
         serializer.save(updated_by=self.request.user)
 
+    def destroy(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+            self.perform_destroy(instance)
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except ProtectedError:
+            return Response({
+                'error': "Manufacturer can't be deleted because it is referenced by existing products.",
+                'code': 'protected_error'
+            }, status=status.HTTP_400_BAD_REQUEST)
+        except Manufacturer.DoesNotExist:
+            return Response({
+                'error': 'Manufacturer not found',
+                'code': 'not_found'
+            }, status=status.HTTP_404_NOT_FOUND)
+
 
 class ManufacturerUpdateAPIView(generics.UpdateAPIView):
     queryset = Manufacturer.objects.all()
