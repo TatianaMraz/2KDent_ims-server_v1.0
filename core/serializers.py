6ce0e8643dcc8642733409a6e7825029902c9b8e
TatianaMraz <tatiana.mrazova@icloud.com>
2024-07-10 10:22:53 +0200

    Product serializer update method added

--- core/serializers.py
+++ core/serializers.py
@@ -50,6 +50,25 @@ class ProductSerializer(serializers.ModelSerializer):
 
         return product
 
+    def update(self, instance, validated_data):
+        supplier_set_data = validated_data.pop('supplier_set', [])
+        instance.name = validated_data.get('name', instance.name)
+        instance.quantity = validated_data.get('quantity', instance.quantity)
+        instance.min_quantity = validated_data.get('min_quantity', instance.min_quantity)
+        instance.expiration_date = validated_data.get('expiration_date', instance.expiration_date)
+        instance.product_type = validated_data.get('product_type', instance.product_type)
+        instance.stock_number = validated_data.get('stock_number', instance.stock_number)
+        instance.image = validated_data.get('image', instance.image)
+        instance.note = validated_data.get('note', instance.note)
+        instance.save()
+
+        # Update supplier sets
+        instance.supplier_set.all().delete()
+        for item in supplier_set_data:
+            SupplierSet.objects.create(product=instance, **item)
+
+        return instance
+
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
@@ -85,5 +104,3 @@ class ManufacturerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Manufacturer
         fields = '__all__'
-
-
