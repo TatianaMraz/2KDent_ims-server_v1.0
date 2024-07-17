25e18b828a01fd6aef2488bc868b566a99a33d93
TatianaMraz <tatiana.mrazova@icloud.com>
2024-07-17 16:20:10 +0200

    deploy

--- database/settings.py
+++ database/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = 'django-insecure-b!%ga3a9u1g)t%r-w)e=#5yn_7ft(4-3gz*g=ld5sb=!_e4a87
 DEBUG = True
 
 # ALLOWED_HOSTS = ['ims-2kdent-d48065238ade.herokuapp.com']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['npgroup-demo-b6684a3e9dff.herokuapp.com']
+# ALLOWED_HOSTS = []
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
@@ -141,7 +142,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",
-    "https://ims-2kdent.web.app"
+    "https://ims-2kdent.web.app",
+    "https://npgroup-demo.web.app"
 ]
 
 REST_FRAMEWORK = {
